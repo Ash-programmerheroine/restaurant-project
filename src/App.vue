8802970b326d8bc11926ba2b067f51fbc418da0c
Ashrafi <ashrafikhanam.18@gmail.com>
2024-01-09 10:41:16 +0600

    added server

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <sign-up></sign-up>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import SignUp from './components/SignUp.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+ components:{
+  SignUp
+
+ }
 }
 </script>
 
@@ -21,6 +23,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
+
 </style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,126 @@
+<template>
+    
+  <img alt="logo" class="logo" src="../assets/restaurant-logo.png">
+   
+  <h1>Sign Up</h1>
+  <form >
+    <div class="form-control">
+      <label for="user-name">Your Name</label>
+      <input id="user-name" name="user-name" type="text" v-model="name" />
+    </div>
+    <div class="form-control">
+      <label for="email">Your Email</label>
+      <input id="user-email" name="user-email" type="text" v-model="email" />
+    </div>
+    <div class="form-control">
+      <label for="age">Your Password</label>
+      <input id="password" name="password" type="number" v-model="password" />
+    </div>
+    <div>
+      <button @click="submitForm">Save Data</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "SignUp",
+  data(){
+    return{
+        name: '',
+        email: '',
+        password: ''
+  }},
+  methods:{
+    async submitForm(){
+      console.log('Username :' + this.name, 'Useremail :' + this.email, 'Userpass :' + this.password);
+      
+      let result = await axios.post("http://localhost:9000/users",{
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      });
+      console.warn(result);
+      if(result.status == 201){
+        alert("SignUp Done")
+      }
+      localStorage.setItem("user-info", JSON.stringify(result.data))
+    }
+  }
+};
+</script>
+<style >
+.logo{
+  width:80px;
+}
+form {
+  margin: 2rem auto;
+  max-width: 20rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: #ffffff;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+input,
+select {
+  display: block;
+  width: 100%;
+  font: inherit;
+  margin-top: 0.5rem;
+}
+
+select {
+  width: auto;
+}
+
+input[type='checkbox'],
+input[type='radio'] {
+  display: inline-block;
+  width: auto;
+  margin-right: 1rem;
+}
+
+input[type='checkbox'] + label,
+input[type='radio'] + label {
+  font-weight: normal;
+}
+input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    margin-bottom: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid#69052A;
+}
+button {
+  font: inherit;
+  border: 1px solid #69052A;
+  background-color: #FFC100;
+  color: #fff;
+  cursor: pointer;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+}
+
+button:hover,
+button:active {
+  border-color: #69052A;
+  background-color: #69052A;
+}
+</style>
